12cbd1bd674a420955dc4e95f325f0accdbbbe59
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-14 12:03:23 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,15 +2,15 @@
 
 
 /**
- * main: this prints the phrase "with proper grammar, but the outcome is a piece of art,"
+ * main: this prints the phrase
  *
- * return: this returns zero to show successfull
+ * Return: this returns zero to show successfull
  *
  */
 
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	
 	return (0);
 }
