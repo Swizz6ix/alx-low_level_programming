cd7112056822e51d2c40c3ae1510f4d32cc8a366
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-14 11:29:52 -0700

    the C programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /**
 *main: prints the phrase
 *
-*Return: This is the return value of the main function and it returns 0 (zero) on success
+*Return: This is the return, it returns 0 (zero) on success
 *
 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
