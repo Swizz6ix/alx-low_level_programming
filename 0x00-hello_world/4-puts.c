67f3a60c44d9a8e32ba4aac985d7353f031eb610
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-14 11:25:41 -0700

    the C programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-*main - prints the phrase
+*main: prints the phrase
 *
-*return -it returns 0 (zero) on success
+*Return: This is the return value of the main function and it returns 0 (zero) on success
 *
 */
 int main(void)
