1adced39d04f03a2a0a8343aa4ad90324629d340
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-15 15:47:54 -0700

    depart

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		purchar(num);
+		if (num < 57)
+		{
+			putchar(',');
+			putchar(' );
+		}
+		num ++
+	putchar('\n')
+	return (0)
