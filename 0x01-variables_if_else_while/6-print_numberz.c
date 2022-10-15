9fd7dc179dcdef889660e30d33fd8a4de9a4a899
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-15 13:35:44 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- prints the numbers
+ *
+ * Return: returns zero if it is successful
+ *
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
