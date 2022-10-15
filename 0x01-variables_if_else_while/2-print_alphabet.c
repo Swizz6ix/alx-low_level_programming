d57726e369a7da74284733597073cb12827a8736
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-14 19:14:59 -0700

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints the letter
+ *
+ * Retrun: returns zero if successful
+ *
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar(`\n);
+	return (0);
+}
