92d3ae2a3834f92bc1bc764ccad2c69c8f451238
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-15 13:05:28 -0700

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q')
+		{
+			letter++;
+		}
+
+		else if (letter == 'e')
+		{
+			letter++;
+		}
+
+		else
+		{
+			putchar(letter);
+			letter;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
