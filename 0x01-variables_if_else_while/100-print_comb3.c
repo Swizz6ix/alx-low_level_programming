a4fda6592efad59ecfffde505e8920c7adb41f1c
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-16 06:36:30 -0700

    double

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-	int firstDigit = 0;
-	int secondDigit= 0;
+	int firstDigit;
+	int secondDigit;
 
-	while ((firstDigit <= 9) & (secondDigit <= 9))
+	while ((firstDigit <= '9') & (secondDigit <= '9'))
 	{
 		putchar(firstDigit);
 		putchar(secondDigit);
