566365e85c83143e625fa2f00efcbe4865bd0510
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-16 09:39:29 -0700

    numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,17 +18,16 @@ int main(void)
 for (firstDigit = '0'; firstDigit <= '9'; firstDigit++)
 
 	{
-
-for (secondDigit = '0'; secondDigit <= '9'; secondDigit++)
+		for (secondDigit = '0'; secondDigit <= '9'; secondDigit++)
 		{
 
 			if ((firstDigit < secondDigit) & (secondDigit <= '9'))
 			{
 				putchar(firstDigit);
 				putchar(secondDigit);
-				
+
 				if ((secondDigit < '9') | (firstDigit < '8'))
-				
+
 				{
 					putchar(',');
 					putchar(' ');
