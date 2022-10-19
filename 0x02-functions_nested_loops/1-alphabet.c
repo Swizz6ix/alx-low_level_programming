d2157a235fddd60e5923bf8d0538a49aa09a23c2
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-18 22:43:27 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,15 +8,13 @@
  * 
  */
 
- 
- int main(void)
+ void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	
+	{
 		_putchar(i);
-	
+	}
 	_putchar('\n');
-
 }
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main- check the code
+ *
+ * Return: returns zero on success
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
