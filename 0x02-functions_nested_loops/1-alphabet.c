a9379f4f44574495379a0b46c59aa267237ae967
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-18 19:45:13 -0700

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- prints the alphabets
+ *
+ * Return: returns zero if it is successful
+ *
+ * 
+ */
+
+ 
+ int main (void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(i);
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory about c functions, there are 16 files in it, each explaining different concept of C functions and nested loops
