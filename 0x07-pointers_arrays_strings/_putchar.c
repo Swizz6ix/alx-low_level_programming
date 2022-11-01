ac3a366035e72282c7e5398e84393694cd5baf67
swizz6ix <ferdinandcharles6@gmail.com>
2022-10-31 21:35:26 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar -writes character c to stdout
+ * @c: char to be printed
+ * Return: returns 1 one on success
+ *
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
