52496d77c678632a0852f1ef032b76fcf2e1926c
swizz6ix <ferdinandcharles6@gmail.com>
2023-04-02 01:08:50 -0700

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,12 +11,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx = 0;
 	char *temp_val = NULL;
-	hash_node_t *temp = NULL;
-	hash_node_t *new = NULL;
+	hash_node_t *temp = NULL, *new = NULL;
 
 	if (ht == NULL || ht->array == NULL || value == NULL)
 		return (0);
-
 	if (strlen(key) == 0 || key == NULL)
 		return (0);
 	temp_val = strdup(value);
