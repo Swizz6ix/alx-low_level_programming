2d4b28b75b8832e3ef3e8b0c957a2f9d04742b76
swizz6ix <ferdinandcharles6@gmail.com>
2023-04-05 06:10:29 -0700

    singly

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,8 +2,6 @@
 #define LISTS_H
 
 /*
- * File: lists.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x12-more_singly_linked_lists directory.
  */
